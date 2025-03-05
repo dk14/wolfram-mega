edb904abe9bf476db0f67a8dbb330aa0a9989063
Dmytro Kondratiuk <dmkndr@gmail.com>
2025-03-05 17:18:07 -0300

    more

--- node.ts
+++ node.ts
@@ -4,11 +4,15 @@ interface Param {
 }
 
 interface OracleCapability {
+    oracleId: OracleId
+    seqNo: number //used for broadcast
     question: string // oracle is responsible for unambiguity of question - this field can be use to match capabilities of different oracles
     params: Param[]
     answers: string[] 
     queryUri: string //how to query oracle
     pubkey: string
+    signature: string
+    pow: HashCashPow[]
 }
 
 
@@ -21,15 +25,16 @@ interface HashCashPow {
     hash: string
 }
 
-type PaymentMethod = string
+type Bid = string
 
 interface OracleId {
     pubkey: string // sign every request/response
+    seqNo: number //used for broadcast
 
-    pow?: HashCashPow
+    pow: HashCashPow[]
     manifestUri?: string
 
-    payment: PaymentMethod
+    bid: Bid
 
     previousId?: OracleId //in case oracle has to change pubkey
     previousIdSignatureToNewPubkey?: string
@@ -45,7 +50,7 @@ interface Oracle {
 }
 
 interface Mempool {
-    oracles: Oracle[]
+    oracles: { [id: string] : Oracle; }
 }
 
 interface FactRequest {
@@ -80,6 +85,7 @@ interface FactConflict {
 interface FactMissing {
     request: FactRequest
     payment?: ProofOfPayment
+    dispute?: Fact
     pow: HashCashPow
 }
 
@@ -104,18 +110,18 @@ interface Api {
     mempool: Mempool
 
     //----exposed as P2P----
-    registerOracle: (id: OracleId) => Promise<Registered | RegistrationError>
-    registerCapability: (cp: OracleCapability) => Promise<Registered | RegistrationError>
-    registerMalleabilityReport: (report: MaleabilityReport) => Promise<ReportAccepted | ReportRejected>
+    announceOracle: (id: OracleId) => Promise<Registered | RegistrationError>
+    announceCapability: (cp: OracleCapability) => Promise<Registered | RegistrationError>
+    reportMalleability: (report: MaleabilityReport) => Promise<ReportAccepted | ReportRejected>
     disputeMissingfactClaim: (claim: FactMissing, fact: Fact) => Promise<DisputeAccepted | DisputeRejected> 
     //note: it does not dispute time delay/SLA, but it is less crushial for most option contracts
 
-    //----exposed as REST-----
+    //----exposed as REST----- oracles should be sorted by proofs of payment
     lookupOracles: (paging: PagingDescriptor, questions: string[]) => Promise<OracleId[]>
     lookupCapabilities: (paging: PagingDescriptor, oracle: OracleId) => Promise<OracleCapability[]>
     lookupReports: (paging: PagingDescriptor, oracle: OracleId) => Promise<OracleCapability[]>
 
-    proxify: (uri: string) => Promise<string>
+    proxify: (req: FactRequest, uri: string) => Promise<string>
 
 }
 
@@ -129,4 +135,101 @@ interface Node {
     processApiRequest: (request: JSON) => Promise<JSON>
     broadcastMessage: (request: JSON) => void
     evict: () => void
+}
+
+interface MempoolConfig {
+
+}
+
+const checkPow = (pow: HashCashPow[], preimage: string): boolean => {
+    return true
+}
+
+const checkCapabilitySignature = (cp: OracleCapability): boolean => {
+    return true
+}
+
+const checkOracleRank = (oracle: OracleId, oracles: OracleId[]): boolean => { 
+    //based on pow difficulty
+    //and proofs of payment
+    //oracles are basicaly bidding with micropayments
+    return true
+}
+
+const checkCapabilityRank = (cp: OracleCapability, o: Oracle): boolean => {
+    return true
+}
+
+const api: Api = {
+    mempool: {
+        oracles: {}
+    },
+    announceOracle: async (id: OracleId): Promise<Registered | RegistrationError> => {
+        if (checkPow(id.pow, id.pubkey)) {
+            if (checkOracleRank(id, Object.values(api.mempool.oracles).map(v => v.id))) {
+                if (api.mempool.oracles[id.pubkey] ?? true) {
+                    api.mempool.oracles[id.pubkey] = {
+                        id,
+                        capabilies: [],
+                        reports: [] 
+                     }
+                     return "success"
+                } else {
+                    //todo merge pows after checking that other fields are same
+                    return "duplicate"
+                }
+                
+            } else {
+                return "rank is too low"
+            }
+        } else {
+            return "wrong pow"
+        }
+        
+    },
+    announceCapability: async (cp: OracleCapability): Promise<Registered | RegistrationError> => {
+        if (checkCapabilitySignature(cp)) {
+            if (checkPow(cp.pow, cp.signature)) {
+                if (checkCapabilityRank(cp, api.mempool.oracles[cp.oracleId.pubkey])) {
+                    api.mempool.oracles[cp.oracleId.pubkey].capabilies.push(cp)
+                    return "success"
+                } else {
+                    return "rank is too low"
+                }
+            } else {
+                return "wrong pow"
+            }
+            
+        } else {
+            return "wrong signature"
+        }
+    },
+    reportMalleability: async (report: MaleabilityReport): Promise<ReportAccepted | ReportRejected> => {
+        return ""
+    },
+    disputeMissingfactClaim: async (claim: FactMissing, fact: Fact): Promise<DisputeAccepted | DisputeRejected> => {
+        const oracle = api.mempool.oracles[claim.request.capability.oracleId.pubkey]
+        if (oracle ?? true) {
+            //find report
+            //augment if found
+            return ""
+        } else {
+            return "unknown oracle"
+        }
+    },
+    lookupOracles: async (paging: PagingDescriptor, questions: string[]): Promise<OracleId[]> => {
+        return []
+    },
+    lookupCapabilities: async (paging: PagingDescriptor, oracle: OracleId): Promise<OracleCapability[]> => {
+        return []
+    },
+    lookupReports: async (paging: PagingDescriptor, oracle: OracleId): Promise<OracleCapability[]> => {
+        return []
+    },
+
+    proxify: async (req: FactRequest, uri: string): Promise<string> => {
+        return ""
+    }
+
+
 }
