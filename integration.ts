6694c38d8cb3d5e79cdfb4880c7066133bb48ec1
Dmytro Kondratiuk <dmkndr@gmail.com>
2025-03-17 07:03:35 -0300

    integration tests

--- integration.ts
+++ integration.ts
@@ -2,6 +2,10 @@ import { ChildProcessWithoutNullStreams, exec, spawn } from 'child_process'
 import waitOn from 'wait-on'
 import * as fs from 'fs'
 import {promisify} from 'util'
+import * as nd from './node'
+import * as pow from './pow'
+import fetch from 'node-fetch'
+import * as assert from 'assert'
 
 const cleanUp = async () => {
     peers.forEach(x => x.proc?.kill())
@@ -57,7 +61,7 @@ const start = async (portP2P: number, portHttp: number, seed: number[]): Promise
     var flag = false
     return new Promise<Peer>((resolve, reject) => 
         child.stdout.on('data', function(data){
-            console.log("" + data);
+            //console.log("" + data);
             
             if (!flag) {
                 resolve({
@@ -73,12 +77,71 @@ const start = async (portP2P: number, portHttp: number, seed: number[]): Promise
 }
 
 
-const peers = await Promise.all(Array.from(Array(5).keys()).map(i => start(8433 + i, 8090 +i, [8433])))
+interface OracleIdWithPk {
+    body: nd.OracleId
+    pk: string
+}
 
-console.log("Waiting for P2P...")
+const genOracle = async (): Promise<OracleIdWithPk> => {
+    const keypair = nd.testOnlyGenerateKeyPair()
+    const oracle: nd.OracleId = {
+        pubkey: keypair.pub,
+        oracleSignature: undefined,
+        seqNo: 0,
+        cTTL: 0,
+        pow: undefined,
+        bid:  {
+            amount: 0,
+            proof: ''
+        },
+        oracleSignatureType: 'SHA256'
+    }
+    oracle.pow = await pow.powOverOracleId(oracle, 2)
+    oracle.oracleSignature = ""
+    oracle.oracleSignature = nd.testOnlySign(JSON.stringify(oracle), keypair.pk)
+    return {
+        body: oracle,
+        pk: keypair.pk
+    }
+}
 
+
+const peers = await Promise.all(Array.from(Array(5).keys()).map(i => start(8433 + i, 8090 + i, [8433])))
+
+console.log("Waiting for P2P network...")
+
+const addr = (p: Peer) => {
+    return 'http://localhost:' + p.port + '/'
+}
 await waitFor(peers.map(p => 'http-get://localhost:' + p.port + '/id'))
+
+console.log("=========TESTING==========")
+
+const oracle1 = await genOracle()
+
+console.log("1) Submit oracle id")
+
+const response1 = await fetch(addr(peers[0]) + 'oracle', {
+	method: 'post',
+	body: JSON.stringify(oracle1.body),
+	headers: {'Content-Type': 'application/json'}
+})
+
+
+const res1 = await response1.text();
+assert.strictEqual(res1, '"success"')
+
+console.log("2) Check oracle id across the nodes")
+
+const responses = await Promise.all(peers.map(p => fetch(addr(peers[0]) + 'oracles')))
+const jsons = await Promise.all(responses.map(r => r.json()))
+const results = jsons.map(j => j as nd.OracleId[])
+
+results.forEach(r => {
+    assert.deepStrictEqual(r, [oracle1.body])
+})
 
 peers.forEach(x => x.proc.kill())
 
+console.log("OK!")
 
