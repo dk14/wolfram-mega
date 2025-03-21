6778b41c03d2949506777174d44ca9a02e46ac27
Dmytro Kondratiuk <dmkndr@gmail.com>
2025-03-20 22:43:07 -0300

    refine

--- src/client-api/oracle-endpoint-api.ts
+++ src/client-api/oracle-endpoint-api.ts
@@ -1,6 +1,6 @@
 import { FactRequest, Commitment, Fact } from "../node";
 
-interface OracleEndpointApi {
+export interface OracleEndpointApi {
     requestCommitment: (req: FactRequest) => Commitment | undefined
     requestFact: (req: Commitment | FactRequest) => Fact
 }
--- src/client-api/trader-api.ts
+++ src/client-api/trader-api.ts
@@ -2,36 +2,50 @@ import { OfferMsg, OracleCapability, OracleId, Report } from "../node";
 
 
 export interface TraderStorage<OracleQuery, CpQuery, RpQuery, MatchingQuery> {
-    addOracle: (cp: OracleId) => void
-    addCp: (cp: OracleCapability) => void
-    addReport: (r: Report) => void
-    addIssuedReport: (r: Report) => void
-    addOffer: (r: OfferMsg) => void
-    addIssuedOffer: (r: OfferMsg) => void
+    addOracle: (cp: OracleId) => Promise<void>
+    addCp: (cp: OracleCapability) => Promise<void>
+    addReport: (r: Report) => Promise<void>
+    addIssuedReport: (r: Report) => Promise<void>
+    addOffer: (r: OfferMsg) => Promise<void>
+    addIssuedOffer: (r: OfferMsg) => Promise<void>
+
+    removeOracles: (pubkeys: string[]) => Promise<void>
+    removeCps: (pubkeys: string[]) => Promise<void>
+    removeReports: (pubkeys: string[]) => Promise<void>
+    removeOffers: (pubkeys: string[]) => Promise<void>
+    removeIssuedOffers: (pubkeys: string[]) => Promise<void>
+    removeIssuedReports: (pubkeys: string[]) => Promise<void>
+
+    queryOracles: (q: OracleQuery) => Promise<OracleId[]>
+    queryCapabilities: (cp: CpQuery) => Promise<OracleCapability[]>
+    queryOffers: (cp: MatchingQuery) => Promise<OfferMsg[]>
+    queryReports: (cp: RpQuery) => Promise<Report[]>
+
+    queryIssuedOffers: (cp: MatchingQuery) => Promise<OfferMsg[]>
+    queryIssuedReports: (cp: RpQuery) => Promise<Report[]>
 
 
-    queryOracles: (q: OracleQuery) => OracleId[]
-    queryCapabilities: (cp: CpQuery) => OracleCapability[]
-    queryOffers: (cp: MatchingQuery) => OfferMsg[]
-    queryReports: (cp: RpQuery) => Report[]
+}
 
-    queryIssuedOffers: (cp: MatchingQuery) => OfferMsg[]
-    queryIssuedReports: (cp: RpQuery) => Report[]
-
+export interface Collector<T> {
+    type: 'OracleId' | 'OracleCapability' | 'Report' | 'OfferMsg'
+    tag: string
+    active: boolean
+    predicate: (cp: T) => boolean
+    cancel: () => Promise<void>
 }
 
 export interface TraderApi {
 
-    collectOracles: (predicate: (cp: OracleId) => boolean) => boolean
-    
-    collectCapabilities: (predicate: (cp: OracleCapability) => boolean) => boolean
+    collectOracles: (tag: string, predicate: (cp: OracleId) => Promise<boolean>) => Promise<Collector<OracleId>>
+    collectCapabilities: (tag: string, predicate: (cp: OracleCapability) => Promise<boolean>) => Promise<Collector<OracleCapability>>
+    collectReports: (tag: string, predicate: (cp: Report) => Promise<boolean>) => Promise<Collector<Report>>
+    collectOffers: (tag: string, matchingPredicate: (cp: OfferMsg) => Promise<boolean>) => Promise<Collector<OfferMsg>>
 
-    issueReport: (cp: Report) => void
-    collectReports: (predicate: (cp: Report) => boolean) => boolean
-    
+    issueReport: (cp: Report) => Promise<void>
 
-    issueOffer: (cp: OfferMsg) => void
-    collectOffers: (matchingPredicate: (cp: OfferMsg) => boolean) => boolean
+    issueOffer: (cp: OfferMsg) => Promise<void>
+   
 
     startBroadcastingIssuedOffers: () => void
     stopBroadcastingIssuedOffers: () => void
